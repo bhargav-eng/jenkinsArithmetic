574f407086b0e12486ace5020037bf1e6d8e1f66
bhargav-eng <245122748064@mvsrec.edu.in>
2025-03-18 12:28:46 +0530

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,7 @@
+public class Integer {
+    public static void main(String[] args) {
+        int num = 42;  // Predefined value for testing
+        System.out.println("You entered: " + num);
+    }
+}
+
